c41d2c9db8954af5c35f4b374decd8b7c43c0559
Mazza <mahery92@hotmail.fr>
2025-05-23 17:25:39 +0200

    added PUT DELETE

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def handle_options():
     if request.method == 'OPTIONS':
         response = jsonify({})
         response.headers['Access-Control-Allow-Origin'] = request.headers.get('Origin', '*')
-        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS, PUT, DELETE'
         response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
         response.headers['Access-Control-Allow-Credentials'] = 'true'
         return response, 200
